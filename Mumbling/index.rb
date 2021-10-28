fc9fdedcd348a90b54ef8e10ceb78163b10bb08b
Iris Perisa <iris.perisa@gmail.com>
2021-10-28 16:01:11 +0200

    feat: adds two refactored version to task Mumbling

--- Mumbling/index.rb
+++ Mumbling/index.rb
@@ -1,4 +1,33 @@
-# version 1
+# Final refactored version
+
+def accum(s)
+  # turn to array
+  letters = s.chars
+  
+  chars = letters.map.with_index do |letter, index|
+    (letter * (index + 1)).capitalize
+  end
+  # outputs Z pp lll...
+  
+  chars.join("-")
+end
+
+# Version 2
+
+def accum(s)
+  # turn to array
+  letters = s.chars
+  i = 0
+  chars = letters.map do |letter|
+    i +=1
+    (letter * i).capitalize
+  end
+  # outputs Z pp lll...
+  
+  chars.join("-")
+end
+
+# Version 1
 
 def accum(s)
   # turn to array
