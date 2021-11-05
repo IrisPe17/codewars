b52503aab8a9b4b6de8b060fc0733e1c65937842
Iris Perisa <iris.perisa@gmail.com>
2021-11-05 15:43:20 +0100

    feat: adds Ruby solution to task Title Case

---
+++ Title Case/index.rb
@@ -0,0 +1,8 @@
+def title_case(title, minor_words = '')
+  minor = minor_words.downcase.split(' ')
+  title_array = title.downcase.capitalize.split(' ')
+  title_array.each do |word|
+    word.capitalize! unless minor.include?(word)
+  end
+  title_array.join(' ')
+end
