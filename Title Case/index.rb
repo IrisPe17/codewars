45d0e676dce9aa62349e0ab033c0bd848a1c0ae2
Iris Perisa <iris.perisa@gmail.com>
2021-11-05 15:44:55 +0100

    fix: removes extra downcase method

--- Title Case/index.rb
+++ Title Case/index.rb
@@ -1,6 +1,6 @@
 def title_case(title, minor_words = '')
   minor = minor_words.downcase.split(' ')
-  title_array = title.downcase.capitalize.split(' ')
+  title_array = title.capitalize.split(' ')
   title_array.each do |word|
     word.capitalize! unless minor.include?(word)
   end
